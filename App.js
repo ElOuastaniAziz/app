bca5f9a1eb6b4403ff8f47a2426aa94e3242536c
Abdel <designsyaz@gmail.com>
2020-10-16 10:59:20 +0200

    Header añadido

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
 import { Appbar } from 'react-native-paper';
+import Constants from 'expo-constants';
 
 export default function App() {
   return (
-    <View>
-      <Appbar style={styles.bottom}>
+    <View style={styles.container}>
+      <StatusBar backgroundColor='#848484' style="auto" />
+      <Appbar>
         <Appbar.Action
           icon="archive"
+          color="#848484"
           onPress={() => console.log('Pressed archive')}
         />
         <Appbar.Action icon="mail" onPress={() => console.log('Pressed mail')} />
@@ -18,9 +21,23 @@ export default function App() {
           onPress={() => console.log('Pressed delete')}
         />
       </Appbar>
-      <View style={styles.container}>
+      <Appbar style={styles.bar_bottom}>
+        <Appbar.Action
+          icon="archive"
+          color="#848484"
+          onPress={() => console.log('Pressed archive')}
+        />
+        <Appbar.Action icon="mail" onPress={() => console.log('Pressed mail')} />
+        <Appbar.Action icon="label" onPress={() => console.log('Pressed label')} />
+        <Appbar.Action
+          icon="delete"
+          onPress={() => console.log('Pressed delete')}
+        />
+      </Appbar>
+      <View style={styles.formulario}>
         <Text>Este rexto ha sido modificado</Text>
-        <StatusBar backgroundColor='#848484' style="auto" />
+        <Button title="Login" color="#848484" style={styles.btn_margin}></Button>
+        <Button title="Register"></Button>
       </View>
     </View>
 
@@ -28,10 +45,24 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
+    flex:1,
+    paddingTop: Constants.statusBarHeight,
+  },
+  formulario: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    margin:15,
+  },
+  bar_bottom: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+
+  btn_margin: {
+    marginTop:3,
   },
 });
