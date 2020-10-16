858dc00f72f2d437d97742e6283b698e85be9a33
Abdel <designsyaz@gmail.com>
2020-10-16 10:13:39 +0200

    bug fixed

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Appbar } from 'react-native-paper';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Este rexto ha sido modificado</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Appbar style={styles.bottom}>
+        <Appbar.Action
+          icon="archive"
+          onPress={() => console.log('Pressed archive')}
+        />
+        <Appbar.Action icon="mail" onPress={() => console.log('Pressed mail')} />
+        <Appbar.Action icon="label" onPress={() => console.log('Pressed label')} />
+        <Appbar.Action
+          icon="delete"
+          onPress={() => console.log('Pressed delete')}
+        />
+      </Appbar>
+      <View style={styles.container}>
+        <Text>Este rexto ha sido modificado</Text>
+        <StatusBar backgroundColor='#848484' style="auto" />
+      </View>
     </View>
+
   );
 }
 
